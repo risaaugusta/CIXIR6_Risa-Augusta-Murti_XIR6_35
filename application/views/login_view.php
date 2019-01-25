5c12a8b919fded50fb00fbb37675c6a9d944125f
Risa Augusta Murti <risa_augusta_26rpl@student.smktelkom-mlg.sch.id>
2019-01-25 09:34:28 +0700

    Tugas CI

---
+++ application/views/login_view.php
@@ -0,0 +1,197 @@
+<!-- <!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>CI Login</title>
+    <link href="<?php echo base_url() ?>assets/css/bootstrap.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class='container' style='margin-top: 100px;'>
+        <div class='row'>
+            <div class='col-md-4'>
+            </div>
+            <div class='col-md-4'>
+                <div class='panel panel-default'>
+                    <div class='panel-body'>
+                        <-- validation message for a successful login -->
+                        <!-- <?php if ($this->session->flashdata('error')) {?>
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                <?php echo $this->session->flashdata('error'); ?>
+                            </div>
+                        <?php  } ?>
+                        <!-- validation messages for taking inputs -->
+                        <?php echo validation_errors('<div class="alert alert-danger alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>','</div>');
+                        ?>
+
+                        <!-- <?php echo form_open('Login/loginUser') ?>
+
+                            <div class="form-group">
+                                <label for="username">Username</label>
+                                <input type="text" class="form-control" name="username" id="username" placeholder="Username">
+                            </div>
+                            <div class="form-group">
+                                <label for="password">Password</label>
+                                <input type="password" class="form-control" name="password" id="password" placeholder="Password">
+                            </div>
+                            <button type="submit" class="btn btn-primary">LOGIN</button>
+                            <a href="<?php echo site_url('Home/Register') ?>" class="btn btn-link">Sign Up</a>
+
+                        <?php echo form_close() ?>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div> -->
+
+    <!-- <script src="<?php echo base_url() ?>assets/js/jquery.min.js"></script>
+    <script src="<?php echo base_url() ?>assets/js/bootstrap.min.js"></script>
+  </body>
+</html> -->
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Bootstrap Login Form with Avatar Image</title>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
+<style type="text/css">
+	body {
+		color: #fff;
+		background: #d47677;
+	}
+	.form-control {
+        min-height: 41px;
+		background: #fff;
+		box-shadow: none !important;
+		border-color: #e3e3e3;
+	}
+	.form-control:focus {
+		border-color: #70c5c0;
+	}
+    .form-control, .btn {        
+        border-radius: 2px;
+    }
+	.login-form {
+		width: 350px;
+		margin: 0 auto;
+		padding: 100px 0 30px;		
+	}
+	.login-form form {
+		color: #7a7a7a;
+		border-radius: 2px;
+        margin-top: 80px;
+    	margin-bottom: 15px;
+        font-size: 13px;
+        background: #ececec;
+        box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
+        padding: 30px;	
+        position: relative;	
+    }
+	.login-form h2 {
+		font-size: 22px;
+        margin: 35px 0 25px;
+    }
+	.login-form .avatar {
+		position: absolute;
+		margin: 0 auto;
+		left: 0;
+		right: 0;
+		top: -50px;
+		width: 95px;
+		height: 95px;
+		border-radius: 50%;
+		z-index: 9;
+		background: #70c5c0;
+		padding: 15px;
+		box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.1);
+	}
+	.login-form .avatar img {
+		width: 100%;
+	}	
+    .login-form input[type="checkbox"] {
+        margin-top: 2px;
+    }
+    .login-form .btn {        
+        font-size: 16px;
+        font-weight: bold;
+		background: #70c5c0;
+		border: none;
+		margin-bottom: 20px;
+    }
+	.login-form .btn:hover, .login-form .btn:focus {
+		background: #50b8b3;
+        outline: none !important;
+	}    
+	.login-form a {
+		color: #fff;
+		text-decoration: underline;
+	}
+	.login-form a:hover {
+		text-decoration: none;
+	}
+	.login-form form a {
+		color: #7a7a7a;
+		text-decoration: none;
+	}
+	.login-form form a:hover {
+		text-decoration: underline;
+	}
+</style>
+</head>
+<body>
+<div class="login-form">
+    <?php echo form_open('Login/loginUser') ?>
+		<div class="avatar">
+			<img src="<?= base_url('assets')?>/img2/avatar.png" alt="Avatar">
+		</div>
+        <?php if ($this->session->flashdata('error')) {?>
+                            <div class="alert alert-danger alert-dismissible" role="alert">
+                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                                    <span aria-hidden="true">&times;</span>
+                                </button>
+                                <?php echo $this->session->flashdata('error'); ?>
+                            </div>
+                        <?php  } ?>
+                        <!-- validation messages for taking inputs -->
+                        <?php echo validation_errors('<div class="alert alert-danger alert-dismissible" role="alert">
+                            <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span></button>','</div>');
+                        ?> 
+        <h2 class="text-center">Member Login</h2>   
+        <!-- validation message for a successful login -->
+                        
+       
+        <div class="form-group">
+        	<input type="text" class="form-control" name="username" placeholder="Username" required="required">
+        </div>
+		<div class="form-group">
+            <input type="password" class="form-control" name="password" placeholder="Password" required="required">
+        </div>        
+        <div class="form-group">
+            <button type="submit" class="btn btn-primary btn-lg btn-block">Sign in</button>
+        </div>
+		<div class="clearfix">
+            <label class="pull-left checkbox-inline"><input type="checkbox"> Remember me</label>
+            <a href="#" class="pull-right">Forgot Password?</a>
+        </div>
+    </form>
+    <p class="text-center small">Don't have an account? <a href="<?php echo site_url('Home/Register') ?>">Sign up here!</a></p>
+    <?php echo form_close() ?>
+</div>
+</body>
+</html>                            
